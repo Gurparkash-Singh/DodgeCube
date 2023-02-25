8e27f0d267df20f85e70cca8af1864f296d97f87
Gurparkash-Singh <singhgk.fateh@gmail.com>
2023-02-25 02:33:11 -0500

    Added Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody player;
+    public float movementSpeed = 4000f;
+    public float sideSpeed = 500f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(-sideSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(sideSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
