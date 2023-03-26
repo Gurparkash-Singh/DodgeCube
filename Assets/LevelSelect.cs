1d55d266b4c78cbcc069e4b352e431022456d853
Beleben <cscottlil03@gmail.com>
2023-03-26 16:27:01 -0400

    Level Select
    added level select screen not working atm

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public AudioSource error;
+
+    public void level_1(){
+
+        // if statement pertruding to main character where if they have a value higher than the level value then they are transported to the new level
+        SceneManager.LoadScene("SampleScene");
+        //else statement that if that doesnt work then we get an error sound
+        error.Play();
+    }
+
+}
